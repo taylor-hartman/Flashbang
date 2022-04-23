87d989c28d07b1bb0fc82e8a4de5aacf780bbb2f
Taylor Hartman <taylor@dhcp-wifi-8021x-168-122-213-94.bu.edu>
2022-04-22 20:29:28 -0400

    auto pinyin

--- app/js/newbunch.js
+++ app/js/newbunch.js
@@ -65,6 +65,13 @@ document.getElementById("title-input").addEventListener("change", () => {
     titleValid(document.getElementById("title-input").value);
 });
 
+document
+    .getElementById("prompt-lang")
+    .addEventListener("change", showPinYinMenu);
+document
+    .getElementById("answer-lang")
+    .addEventListener("change", showPinYinMenu);
+
 //#endregion
 
 //#region HTML Management
@@ -130,6 +137,19 @@ function generatePairsHTML() {
             </div>`;
         document.getElementById("pair-container").appendChild(newPair);
     }
+
+    showPinYinMenu(); //showpinyinmenu must be done after generatePairsHTML
+
+    //adds event listeners after clear
+    if (document.getElementById("auto-pinyin-prompt").checked) {
+        const htmlPrompts = document.getElementsByClassName("prompt");
+        addPinyinInputListeners(htmlPrompts);
+    }
+
+    if (document.getElementById("auto-pinyin-answer").checked) {
+        const htmlAnswers = document.getElementsByClassName("answer");
+        addPinyinInputListeners(htmlAnswers);
+    }
 }
 
 function refactorIndicies() {
@@ -137,6 +157,70 @@ function refactorIndicies() {
     for (x = 0; x < indicies.length; x++) {
         indicies[x].innerText = x + 1;
     }
+}
+
+function showPinYinMenu() {
+    //called at end of generatePairsHTML()
+    let menuShown = false;
+    if (
+        document.getElementById("prompt-lang").value == "zh-CN" ||
+        document.getElementById("prompt-lang").value == "zh-HK" ||
+        document.getElementById("prompt-lang").value == "zh-TW"
+    ) {
+        document.getElementById("pinyin-menu").classList.remove("undisplay");
+        document.getElementById("pinyin-half-prompt").classList.remove("hide");
+        menuShown = true;
+    }
+
+    if (
+        document.getElementById("answer-lang").value == "zh-CN" ||
+        document.getElementById("answer-lang").value == "zh-HK" ||
+        document.getElementById("answer-lang").value == "zh-TW"
+    ) {
+        document.getElementById("pinyin-menu").classList.remove("undisplay");
+        document.getElementById("pinyin-half-answer").classList.remove("hide");
+        menuShown = true;
+    }
+
+    if (!menuShown) {
+        document.getElementById("pinyin-menu").classList.add("undisplay");
+        document.getElementById("pinyin-half-prompt").classList.add("hide");
+        document.getElementById("pinyin-half-answer").classList.add("hide");
+    }
+}
+
+document.getElementById("auto-pinyin-prompt").addEventListener("change", () => {
+    const htmlPrompts = document.getElementsByClassName("prompt");
+    if (document.getElementById("auto-pinyin-prompt").checked) {
+        addPinyinInputListeners(htmlPrompts);
+    } else {
+        removePinyinInputListeners(htmlPrompts);
+    }
+});
+
+document.getElementById("auto-pinyin-answer").addEventListener("change", () => {
+    const htmlAnswers = document.getElementsByClassName("answer");
+    if (document.getElementById("auto-pinyin-answer").checked) {
+        addPinyinInputListeners(htmlAnswers);
+    } else {
+        removePinyinInputListeners(htmlAnswers);
+    }
+});
+
+function removePinyinInputListeners(elemList) {
+    for (x = 0; x < elemList.length; x++) {
+        elemList[x].removeEventListener("blur", addPinYinText);
+    }
+}
+
+function addPinyinInputListeners(elemList) {
+    for (x = 0; x < elemList.length; x++) {
+        addPinyinInputListener(elemList[x]);
+    }
+}
+
+function addPinyinInputListener(elem) {
+    elem.addEventListener("blur", addPinYinText);
 }
 
 // generateLangHTML();
@@ -303,7 +387,7 @@ function refactorIndicies() {
 //         "Thailand",
 //         "Turkey",
 //         "China",
-//         "Hond Kong",
+//         "Hong Kong",
 //         "Taiwan",
 //     ];
 //     let langhtml = "";
@@ -390,6 +474,14 @@ document.getElementById("plus").addEventListener("click", (e) => {
                 <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 6.586c-.78-.781-2.048-.781-2.828 0l-2.586 2.586-2.586-2.586c-.78-.781-2.048-.781-2.828 0-.781.781-.781 2.047 0 2.828l2.585 2.586-2.585 2.586c-.781.781-.781 2.047 0 2.828.39.391.902.586 1.414.586s1.024-.195 1.414-.586l2.586-2.586 2.586 2.586c.39.391.902.586 1.414.586s1.024-.195 1.414-.586c.781-.781.781-2.047 0-2.828l-2.585-2.586 2.585-2.586c.781-.781.781-2.047 0-2.828z"/></svg>
         </a>
         </div>`;
+
+    if (document.getElementById("auto-pinyin-prompt").checked) {
+        addPinyinInputListener(newPair.getElementsByClassName("prompt")[0]);
+    }
+
+    if (document.getElementById("auto-pinyin-answer").checked) {
+        addPinyinInputListener(newPair.getElementsByClassName("answer")[0]);
+    }
 
     if (editingPairs) {
         const deleteBtn = newPair.getElementsByClassName("pair-delete-btn")[0];
@@ -572,6 +664,22 @@ document
 /* -------------------------------------------------------------------------- */
 /*                           parsing and validation                           */
 /* -------------------------------------------------------------------------- */
+
+function addPinYinText(e) {
+    let val = e.target.value;
+    if (val.trim() != "") {
+        if (val.includes("(") && val.includes(")")) {
+            const rmp = /\(.*?\)/g; //removes parenthesis and text btw them
+            val = val.replace(rmp, "").trim();
+        }
+        // * @param str Chinese character to be converted
+        // * @param splitter separated characters, separated by spaces by default
+        // * @param withtone return Whether the result contains tones, the default is
+        // * @param polyphone Whether polyphone supports polyphones, the default is no
+        e.target.value =
+            val + ` (${pinyinUtil.getPinyin(val, " ", true, false)})`;
+    }
+}
 
 let titleTimeout;
 function titleValid(t) {
